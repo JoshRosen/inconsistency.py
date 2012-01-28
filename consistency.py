1e4dde8c85502d16120922a334102f5edaa51d44
Josh Rosen <rosenville@gmail.com>
2012-01-27 23:48:06 -0800

    Add context-aware capitalization checks.
    These should catch single-word capitalization inconsistencies, while
    ignoring false-positives caused by capitalized words at the
    beginning of sentences.

--- consistency.py
+++ consistency.py
@@ -1,6 +1,10 @@
 import sys
+import string
 from collections import defaultdict
+import nltk
+nltk.download('punkt', quiet=True)
 from nltk.tokenize.treebank import TreebankWordTokenizer
+from nltk.tokenize.punkt import PunktSentenceTokenizer
 from nltk.util import ingrams, bigrams
 
 
@@ -21,17 +25,18 @@ def strip_common_fixes(a, b):
     return (x, y)
 
 
-def lower_case(x):
+def is_uppercase(x):
+    return x and x[0] in string.uppercase
+
+
+def canonicalize(ngram):
     """
-    >>> lower_case("Apple")
-    'apple'
-    >>> lower_case("Batch Gradient")
-    'Batch Gradient'
+    Produce dictionary keys for ngrams, so that potential inconsistencies
+    map to the same key.
     """
-    if len(x.split()) == 1:
-        return x[0].lower() + "".join(x[1:])
-    else:
-        return x
+    if ngram is str:
+        ngram = (ngram, )
+    return "".join(x.lower() for x in ngram).strip(",. ").replace("-", "")
 
 
 def consistency(s):
@@ -40,23 +45,68 @@ def consistency(s):
     ...                 "... in Batch Gradient Descent ...")
     >>> [(x, sorted(y)) for (x, y) in m.items() if len(y) >= 2]
     [('gradientdescent', ['Gradient Descent', 'gradient descent'])]
+
+    >>> m = consistency("This sentence's first word appears uncapitalized in "
+    ...                 "this sentence.  Hadoop should be capitalized as "
+    ...                 " Hadoop, not hadoop.")
+    >>> [(x, sorted(y)) for (x, y) in m.items() if len(y) >= 2]
+    [('hadoop', ['Hadoop', 'hadoop'])]
+
+    If the second word of a sentence is capitalized, it will be be considered
+    if and only if the following word is uncapitalized:
+
+    >>> m = consistency("The Operator may be replaced by another operator")
+    >>> [(x, sorted(y)) for (x, y) in m.items() if len(y) >= 2]
+    [('operator', ['Operator', 'operator'])]
+    >>> m = consistency("The Operator Descriptor describes an operator")
+    >>> [(x, sorted(y)) for (x, y) in m.items() if len(y) >= 2]
+    []
+
     """
+    sent_tokenizer = PunktSentenceTokenizer()
     tokenizer = TreebankWordTokenizer()
-    tokens = tokenizer.tokenize(s)
     mappings = defaultdict(set)
-    # Map normalized ngrams
-    for x in range(1, 10):
-        for ngram in ingrams(tokens, x):
-            norm = "".join(x.lower() for x in ngram).strip(",. ").replace("-", "")
-            source = " ".join(ngram).strip(",. ")
+
+    sentences = sent_tokenizer.tokenize(s)
+    for sent in sentences:
+        tokens = tokenizer.tokenize(sent)
+        # The capitalization of individual words poses a problem: we would like
+        # to detect cases where names are miscapitalized (e.g. hadoop instead
+        # of Hadoop), but we want to avoid false-positives due to capitalized
+        # words that start a sentence or are part of capitalized phrases.
+
+        # Therefore, we only add mappings for capitalized unigrams if they do
+        # not start a sentence and are not adjacent to other capitalized words.
+        for i in range(1, len(tokens)):
+            prev_token = tokens[i-1]
+            token = tokens[i]
+            if i+1 < len(tokens):
+                next_token = tokens[i+1]
+            else:
+                next_token = ""
+            adjacent_uppercase = (i > 1 and is_uppercase(prev_token)) or \
+                                 is_uppercase(next_token)
+            if is_uppercase(token) and adjacent_uppercase:
+                continue
+            norm = canonicalize(token)
+            source = token.strip(",. ")
             mappings[norm].add(source)
+        # Map normalized ngrams
+        for x in range(2, 10):
+            for ngram in ingrams(tokens, x):
+                norm = canonicalize(ngram)
+                source = " ".join(ngram).strip(",. ")
+                if len(source.split()) == x:
+                    mappings[norm].add(source)
+
     # For normalized forms with mutiple values, filter out longer ngrams that
-    # may be covered by shorter ones or that are trivial capitalization differences
+    # may be covered by shorter ones or that are trivial capitalization
+    # differences
     for (key, values) in mappings.items():
         if len(values) > 1:
             for (a, b) in bigrams(values):
-                (x, y) = [" ".join(x) for x in strip_common_fixes(lower_case(a).split(),
-                                                                  lower_case(b).split())]
+                (x, y) = [" ".join(x) for x in strip_common_fixes(a.split(),
+                                                                  b.split())]
                 if (x, y) != (a, b):
                     del mappings[key]
                     break
